9ba67d13681d192d371083340d5a86202f3ec96a
SaadMansuri3 <saadmansurithree@gmail.com>
2021-09-13 17:46:05 +0530

    Added Registration/Security Layer

--- NatureFresh/NatureFresh/Site.Master.cs
+++ NatureFresh/NatureFresh/Site.Master.cs
@@ -11,7 +11,34 @@ namespace NatureFresh
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (Session["UsernameHolder"] != null)
+            {
+                RegisterBtn.Visible = false;
+                NavbarUsernameLabel.Text = Session["UsernameHolder"].ToString();
+            }
+            else
+            {
+                LogOutBtn.Visible = false;
+                RegisterBtn.Visible = true;
+                NavbarUsernameLabel.Visible = false ;
+            }
+        }
+
+        protected void RegisterBtn_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
+        }
+
+            protected void LogOutBtn_Click(object sender, EventArgs e)
+        {
+            if (Session["UsernameHolder"] != null)
+            {
+                LogOutBtn.Visible = true;
+                Session.Abandon();
+                Response.Redirect("Default.aspx");
+            }
 
         }
+
     }
 }
